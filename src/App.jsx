5477bb636d4ca0f80ed64c99826d7d820b89c319
abdulrahaman12 <abdulrahaman7170@gmail.com>
2025-12-16 20:31:38 +0530

    Input Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Input from "./components/input/Input";
+
+function App() {
+  return (
+    <>
+      <Input />
+    </>
+  );
+}
+
+export default App;
